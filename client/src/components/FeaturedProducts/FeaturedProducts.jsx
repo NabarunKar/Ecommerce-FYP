20f60a7f46fbd19e7605da0b61e9db0fad8eef62
NabarunKar <nabarunkar01@gmail.com>
2023-05-24 20:36:31 +0530

    first commit

---
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Card from "../Card/Card";
+import "./FeaturedProducts.scss";
+import useFetch from "../../hooks/useFetch";
+
+const FeaturedProducts = ({ type }) => {
+  const { data, loading, error } = useFetch(
+    `/products?populate=*&[filters][type][$eq]=${type}`
+  );
+
+  return (
+    <div className="featuredProducts">
+      <div className="top">
+        <h1>{type} products</h1>
+        <p>
+        Introducing our Featured Collection: Unleash Your Style!
+
+Discover the ultimate destination for fashion-forward individuals with our carefully curated Featured Collection. From head-turning apparel to stunning accessories, we have handpicked the latest trends and must-have items to help you express your unique style.
+        </p>
+      </div>
+      <div className="bottom">
+        {error
+          ? "Something went wrong!"
+          : loading
+          ? "loading"
+          : data?.map((item) => <Card item={item} key={item.id} />)}
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedProducts;
